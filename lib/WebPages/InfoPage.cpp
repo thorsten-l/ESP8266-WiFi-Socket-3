77093ff066b2f9f3c22ef203c14c28b6ff274b72
Thorsten Ludewig <th@l9g.de>
2021-04-09 11:26:17 +0200

    release 3.0.0

--- lib/WebPages/InfoPage.cpp
+++ lib/WebPages/InfoPage.cpp
@@ -17,6 +17,8 @@ void handleInfoPage()
 
   sendLegend("RESTful API");
 
+  sendPrint("<p><b>Legacy API</b> - Returns in JSON format</p>");
+
   sendPrintf(
       "<p><a href='http://%s/info'>http://%s/info</a> - ESP8266 Info</p>",
       WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
@@ -30,9 +32,63 @@ void handleInfoPage()
       WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
  
   sendPrintf(
-      "<p><a href='http://%s/state'>http://%s/state</a> - Socket JSON status (0 or 1)</p>",
+      "<p><a href='http://%s/state'>http://%s/state</a></p>",
       WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
  
+  sendPrint("<p>&nbsp;</p>");
+  sendPrint("<p><b>JSON API</b> - Returns status in JSON format</p>");
+
+  sendPrintf(
+      "<p>http://%s/api/json - HTTP_POST socket (ON or OFF)</p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
+  sendPrintf(
+      "<p><a href='http://%s/api/json'>http://%s/api/json</a> - HTTP_GET</p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
+  sendPrintf(
+      "<p>http://%s/cmd - HTTP_POST (ON or OFF)</p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
+  sendPrintf(
+      "<p><a href='http://%s/cmd/ON'>http://%s/cmd/ON</a></p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+ 
+  sendPrintf(
+      "<p><a href='http://%s/cmd/OFF'>http://%s/cmd/OFF</a></p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
+  sendPrintf(
+      "<p><a href='http://%s/state'>http://%s/state</a></p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
+  sendPrint("<p>&nbsp;</p>");
+  sendPrint("<p><b>Plain text API</b> - Returns status in plain text format</p>");
+
+  sendPrintf(
+      "<p>http://%s/api/plain - HTTP_POST socket (ON or OFF)</p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
+  sendPrintf(
+      "<p><a href='http://%s/api/plain'>http://%s/api/plain</a> - HTTP_GET</p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
+  sendPrintf(
+      "<p>http://%s/plain/cmd - HTTP_POST (ON or OFF)</p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
+  sendPrintf(
+      "<p><a href='http://%s/plain/cmd/ON'>http://%s/plain/cmd/ON</a> - Socket ON</p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+ 
+  sendPrintf(
+      "<p><a href='http://%s/plain/cmd/OFF'>http://%s/plain/cmd/OFF</a> - Socket OFF</p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
+  sendPrintf(
+      "<p><a href='http://%s/plain/state'>http://%s/plain/state</a></p>",
+      WiFi.localIP().toString().c_str(), WiFi.localIP().toString().c_str());
+
   sendLegend("Services");
 
   sendPrintf("<p>OTA Enabled: %s</p>",
