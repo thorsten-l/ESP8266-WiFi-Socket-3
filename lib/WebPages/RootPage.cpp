cdc260f7c82f2bb3b46e59356d195aab4b4b8177
Thorsten Ludewig <th@l9g.de>
2021-04-22 14:43:30 +0200

    WiFi signal strength added

--- lib/WebPages/RootPage.cpp
+++ lib/WebPages/RootPage.cpp
@@ -30,21 +30,25 @@ void handleRootPage()
   sprintf(titleBuffer, APP_NAME " - %s", appcfg.ota_hostname);
   sendHeader(titleBuffer);
 
-  sendPrint("<form class='pure-form pure-form-aligned'><fieldset>");
+  sendPrint_P(PSTR("<form class='pure-form pure-form-aligned'><fieldset>"));
 
-  sendLegend("Current Status");
+  sendLegend_P(PSTR("Current Status"));
   sendPrintf("<div id='idpwr' class='pure-button' "
                    "style='background-color: #%s'>Power is %s</div>",
                    webPowerState ? "80ff80" : "ff8080",
                    webPowerState ? "ON" : "OFF");
 
-  sendLegend("Actions");
-  sendPrint(
+  sendLegend_P(PSTR("Actions"));
+  sendPrint_P(PSTR(
       "<a href=\"/?power=ON\" class=\"pure-button button-on\">ON</a>"
-      "<a href=\"/?power=OFF\" class=\"pure-button button-off\">OFF</a>");
+      "<a href=\"/?power=OFF\" class=\"pure-button button-off\">OFF</a>"));
+
+  sendLegend_P(PSTR("WiFi signal"));
+  sendPrint_P(PSTR("<span id='wifi' style='vertical-align:middle;display:inline-block;width:48px;height:48px' class='wifi4'></span>" ));
+  sendPrint_P(PSTR("<span id='wsig'></span>" ));
 
 #if defined(HAVE_ENERGY_SENSOR) && defined(HAVE_HLW8012)
-  sendLegend("Energy Sensor");
+  sendLegend_P("Energy Sensor");
   int rid=0;
   char valueBuffer[32];
 
@@ -56,10 +60,10 @@ void handleRootPage()
   sendTextGroupReadOnly( rid++, "Power", valueBuffer );
 #endif
 
-  sendPrint("</fieldset></form>\n");
+  sendPrint_P(PSTR("</fieldset></form>\n"));
 
 #ifdef HAVE_ENERGY_SENSOR
-  sendPrint( "<script>function getPowerState(){"
+  sendPrint_P( PSTR("<script>function getPowerState(){"
     "var v=document.getElementById('pgid0'),"
         "c=document.getElementById('pgid1'),"
         "p=document.getElementById('pgid2');"
@@ -75,15 +79,40 @@ void handleRootPage()
          "s.textContent=\"Power is OFF\";"
          "s.style=\"background-color: #ff8080\";"
       "}"
-    "})}setInterval(getPowerState,5e3);</script>" );
+    "})}setInterval(getPowerState,5e3);</script>" ));
 #else
-  sendPrint(
-      "<script>function getPowerState(){var e = "
-      "document.getElementById(\"idpwr\");fetch(\"/state\").then((resp) => "
-      "resp.json()).then(function(data){if(data.state===\"ON\"){e.textContent="
-      "\"Power is ON\";e.style=\"background-color: #80ff80\";} else "
-      "{e.textContent=\"Power is OFF\";e.style=\"background-color: "
-      "#ff8080\";}});} setInterval(getPowerState,10000);</script>");
+  sendPrint_P(PSTR(
+    "<script>"
+      "function getPowerState(){"
+        "var e = document.getElementById(\"idpwr\");"
+        "fetch(\"/state\").then((resp) => resp.json()).then(function(data){"
+          "if(data.state===\"ON\"){"
+            "e.textContent=\"Power is ON\";"
+            "e.style=\"background-color: #80ff80\";"
+          "}"
+          "else {"
+            "e.textContent=\"Power is OFF\";"
+            "e.style=\"background-color:#ff8080\";"
+          "}"
+        "})"
+      "}"
+      "setInterval(getPowerState,10000);"
+    "</script>"));
 #endif
+
+  sendPrint_P(PSTR(
+    "<script>"
+      "function getInfo(){"
+        "var wi = document.getElementById(\"wifi\");"
+        "var ws = document.getElementById(\"wsig\");"
+        "fetch(\"/info\").then((resp) => resp.json()).then(function(info){"
+          "var i = Math.min(Math.trunc(info.wifi_signal/25)+1,4);"
+          "ws.innerHTML= ' ' + info.wifi_signal + '%';"
+          "wi.className='wifi' + i;"
+        "})"
+      "}"
+      "getInfo();"
+      "setInterval(getInfo,61000);"
+    "</script>"));
   sendFooter();
 }
